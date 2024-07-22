1755e57ec8417a97f7d64878fda904807453bad2
Winz-Mawin7 <Winz.Mawin7@gmail.com>
2024-07-22 21:27:29 +0700

    feat: use react-hook-form in steps form (wip)

--- src/app/(routes)/plan/create/page.tsx
+++ src/app/(routes)/plan/create/page.tsx
@@ -3,10 +3,28 @@
 import PersonalInfoForm from '@/components/form/PersonalInfoForm'
 import WorkoutGoalForm from '@/components/form/WorkoutGoalForm'
 import WorkoutPlanForm from '@/components/form/WorkoutPlanForm'
+import { useForm, FormProvider } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { PersonalInfoData } from '@/components/form'
 import { useCreatePlan } from '@/data/plan.query'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import * as z from 'zod'
+
+export const createPlanFormSchema = z.object({
+  personalInfo: z.object({
+    planName: z.string().min(1, 'Plan name is required'),
+    birthdate: z.string().min(1, 'Date of birth is required'),
+    height: z.number().min(1, 'Height is required'),
+    weight: z.number().min(1, 'Weight is required'),
+    weeklyActivities: z.array(z.string()),
+  }),
+  weeklyActivities: z.array(z.string()),
+  workoutGoal: z.string().min(1, 'Workout goal is required'),
+  workoutPlan: z.string(),
+})
+
+export type CreatePlanFormData = z.infer<typeof createPlanFormSchema>
 
 export default function CreatePlanPage() {
   const router = useRouter()
@@ -15,6 +33,22 @@ export default function CreatePlanPage() {
   const [step, setStep] = useState(1)
   const [personalInfo, setPersonalInfo] = useState<PersonalInfoData | null>(null)
   const [workoutGoal, setworkoutGoal] = useState<string>('')
+
+  const methods = useForm<CreatePlanFormData>({
+    resolver: zodResolver(createPlanFormSchema),
+    mode: 'all',
+    defaultValues: {
+      personalInfo: {
+        planName: '',
+        birthdate: '',
+        height: 0,
+        weight: 0,
+      },
+      weeklyActivities: [],
+      workoutGoal: '',
+      workoutPlan: '',
+    },
+  })
 
   const handlePersonalInfoSubmit = (data: PersonalInfoData) => {
     setPersonalInfo(data)
@@ -46,22 +80,28 @@ export default function CreatePlanPage() {
     }
   }
 
+  const onSubmit = (data: CreatePlanFormData) => {
+    console.log(data)
+  }
+
   return (
-    <div className="container relative mx-auto">
-      <div className="mx-4 my-12 mt-8 rounded bg-white px-4 py-8">
-        {step === 1 && <PersonalInfoForm onSubmit={handlePersonalInfoSubmit} />}
-        {step === 2 && personalInfo && (
-          <WorkoutGoalForm personalInfo={personalInfo} onSubmit={handleworkoutGoalSubmit} goBack={goBack} />
-        )}
-        {step === 3 && personalInfo && (
-          <WorkoutPlanForm
-            personalInfo={personalInfo}
-            workoutGoal={workoutGoal}
-            onSubmit={handleWorkoutPlanSubmit}
-            goBack={goBack}
-          />
-        )}
+    <FormProvider {...methods}>
+      <div className="container relative mx-auto">
+        <form onSubmit={methods.handleSubmit(onSubmit)} className="mx-4 my-12 mt-8 rounded bg-white px-4 py-8">
+          {step === 1 && <PersonalInfoForm onSubmit={handlePersonalInfoSubmit} />}
+          {step === 2 && personalInfo && (
+            <WorkoutGoalForm personalInfo={personalInfo} onSubmit={handleworkoutGoalSubmit} goBack={goBack} />
+          )}
+          {step === 3 && personalInfo && (
+            <WorkoutPlanForm
+              personalInfo={personalInfo}
+              workoutGoal={workoutGoal}
+              onSubmit={handleWorkoutPlanSubmit}
+              goBack={goBack}
+            />
+          )}
+        </form>
       </div>
-    </div>
+    </FormProvider>
   )
 }
--- src/components/form/PersonalInfoForm.tsx
+++ src/components/form/PersonalInfoForm.tsx
@@ -1,12 +1,10 @@
 // components/PersonalInfoForm.tsx
 'use client'
 
+import { CreatePlanFormData } from '@/app/(routes)/plan/create/page'
+import { useFieldArray, useFormContext } from 'react-hook-form'
 import { PlusCircleIcon } from '@heroicons/react/24/outline'
-import React, { useState } from 'react'
-
-interface PersonalInfoFormProps {
-  onSubmit: (data: PersonalInfoData) => void
-}
+import React, { useEffect, useState } from 'react'
 
 export interface PersonalInfoData {
   planName: string
@@ -16,97 +14,146 @@ export interface PersonalInfoData {
   weeklyActivities: string[]
 }
 
-const PersonalInfoForm: React.FC<PersonalInfoFormProps> = ({ onSubmit }) => {
-  const [formData, setFormData] = useState<PersonalInfoData>({
-    planName: '',
-    birthdate: '',
-    height: 0,
-    weight: 0,
-    weeklyActivities: [''],
+type PersonalInfoFormProps = {
+  onNext: () => void
+}
+
+const PersonalInfoForm: React.FC<PersonalInfoFormProps> = ({ onNext }: PersonalInfoFormProps) => {
+  const [isPersonalInfoValid, setIsPersonalInfoValid] = useState(false)
+  const {
+    register,
+    formState: { errors, isValid },
+    watch,
+    trigger,
+    control,
+  } = useFormContext<CreatePlanFormData>()
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'personalInfo.weeklyActivities',
   })
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value })
-  }
+  const personalInfo = watch('personalInfo')
+  console.log('🚀 ~ personalInfo:', personalInfo)
 
-  const handleActivityChange = (index: number, value: string) => {
-    const newActivities = [...formData.weeklyActivities]
-    newActivities[index] = value
-    setFormData({ ...formData, weeklyActivities: newActivities })
-  }
+  useEffect(() => {
+    const validatePersonalInfo = async () => {
+      const result = await trigger('personalInfo', { shouldFocus: true })
+      console.log('🚀 ~ validatePersonalInfo ~ result:', result)
+      setIsPersonalInfoValid(result)
+    }
 
-  const addActivity = () => {
-    setFormData({ ...formData, weeklyActivities: [...formData.weeklyActivities, ''] })
-  }
+    validatePersonalInfo()
+  }, [trigger, personalInfo.planName])
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    onSubmit(formData)
+  // const [formData, setFormData] = useState<PersonalInfoData>({
+  //   planName: '',
+  //   birthdate: '',
+  //   height: 0,
+  //   weight: 0,
+  //   weeklyActivities: [''],
+  // })
+
+  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setFormData({ ...formData, [e.target.name]: e.target.value })
+  // }
+
+  // const handleActivityChange = (index: number, value: string) => {
+  //   const newActivities = [...formData.weeklyActivities]
+  //   newActivities[index] = value
+  //   setFormData({ ...formData, weeklyActivities: newActivities })
+  // }
+
+  // const addActivity = () => {
+  //   setFormData({ ...formData, weeklyActivities: [...formData.weeklyActivities, ''] })
+  // }
+
+  // const handleSubmit = (e: React.FormEvent) => {
+  //   e.preventDefault()
+  //   onSubmit(formData)
+  // }
+
+  const handleNext = async () => {
+    onNext()
   }
 
   return (
     <div className="flex flex-col gap-4">
       <h2 className="text-lg font-bold">Personal Info</h2>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-        <div>
-          <label htmlFor="planName" className="label label-text">
-            Plan Name
-          </label>
-          <input
-            type="text"
-            id="planName"
-            name="planName"
-            value={formData.planName}
-            onChange={handleChange}
-            required
-            className="input input-bordered w-full"
-          />
-        </div>
-        <div>
-          <label htmlFor="birthdate" className="label label-text">
-            Date of Birth
-          </label>
-          <input
-            type="date"
-            id="birthdate"
-            name="birthdate"
-            value={formData.birthdate}
-            onChange={handleChange}
-            required
-            className="input input-bordered w-full"
-          />
-        </div>
-        <div>
-          <label htmlFor="height" className="label label-text">
-            Height (cm)
-          </label>
-          <input
-            type="number"
-            id="height"
-            name="height"
-            value={formData.height}
-            onChange={handleChange}
-            required
-            className="input input-bordered w-full"
-          />
-        </div>
-        <div>
-          <label htmlFor="weight" className="label label-text">
-            Weight (kg)
-          </label>
-          <input
-            type="number"
-            id="weight"
-            name="weight"
-            value={formData.weight}
-            onChange={handleChange}
-            required
-            className="input input-bordered w-full"
-          />
-        </div>
-        <div className="flex flex-col gap-4">
-          <label className="label label-text">Weekly Activities</label>
-          {formData.weeklyActivities.map((activity, index) => (
+      <div>
+        <label htmlFor="planName" className="label label-text">
+          Plan Name
+        </label>
+        <input
+          id="planName"
+          type="text"
+          className="input input-bordered w-full"
+          {...register('personalInfo.planName')}
+        />
+        {errors.personalInfo?.planName && <p className="text-red-500">{errors.personalInfo.planName.message}</p>}
+      </div>
+      <div>
+        <label htmlFor="birthdate" className="label label-text">
+          Date of Birth
+        </label>
+        <input
+          type="date"
+          id="birthdate"
+          // name="birthdate"
+          // value={formData.birthdate}
+          // onChange={handleChange}
+          // required
+          className="input input-bordered w-full"
+          {...register('personalInfo.birthdate')}
+        />
+        {errors.personalInfo?.birthdate && <p className="text-red-500">{errors.personalInfo.birthdate.message}</p>}
+      </div>
+      <div>
+        <label htmlFor="height" className="label label-text">
+          Height (cm)
+        </label>
+        <input
+          type="number"
+          id="height"
+          // name="height"
+          // value={formData.height}
+          // onChange={handleChange}
+          // required
+          className="input input-bordered w-full"
+          {...register('personalInfo.height', { valueAsNumber: true })}
+        />
+        {errors.personalInfo?.height && <p className="text-red-500">{errors.personalInfo.height.message}</p>}
+      </div>
+      <div>
+        <label htmlFor="weight" className="label label-text">
+          Weight (kg)
+        </label>
+        <input
+          type="number"
+          id="weight"
+          // name="weight"
+          // value={formData.weight}
+          // onChange={handleChange}
+          // required
+          className="input input-bordered w-full"
+          {...register('personalInfo.weight', { valueAsNumber: true })}
+        />
+        {errors.personalInfo?.weight && <p className="text-red-500">{errors.personalInfo.weight.message}</p>}
+      </div>
+      <div className="flex flex-col gap-4">
+        <label className="label label-text">Weekly Activities</label>
+        {fields.map((field, index) => (
+          <div key={field.id} className="mb-2 flex">
+            <input
+              {...register(`personalInfo.weeklyActivities.${index}`)}
+              className="mr-2 flex-grow rounded border p-2"
+            />
+            <button type="button" onClick={() => remove(index)} className="btn">
+              Remove
+            </button>
+          </div>
+        ))}
+
+        {/* {formData.weeklyActivities.map((activity, index) => (
             <input
               key={index}
               type="text"
@@ -114,15 +161,20 @@ const PersonalInfoForm: React.FC<PersonalInfoFormProps> = ({ onSubmit }) => {
               onChange={(e) => handleActivityChange(index, e.target.value)}
               className="input input-bordered w-full"
             />
-          ))}
-        </div>
-        <button type="button" onClick={addActivity} className="btn btn-outline btn-sm w-fit text-xs text-gray-500">
-          Add activity <PlusCircleIcon className="h-5 w-5" />
-        </button>
-        <button type="submit" className="btn btn-neutral mt-4 w-full">
-          Next
-        </button>
-      </form>
+          ))} */}
+      </div>
+      <button type="button" onClick={() => append('')} className="btn btn-outline btn-sm w-fit text-xs text-gray-500">
+        Add activity <PlusCircleIcon className="h-5 w-5" />
+      </button>
+
+      <button
+        type="submit"
+        className="btn btn-neutral mt-4 w-full"
+        onClick={handleNext}
+        disabled={!isPersonalInfoValid}
+      >
+        Next
+      </button>
     </div>
   )
 }
